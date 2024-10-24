2750a9213a0f6664a878d50e722ae164b7d1ff2c
BonnyYangC <bonny.yang1981@gmail.com>
2024-10-24 15:14:09 +1100

    add test workflow

--- test/maps-search.test.ts
+++ test/maps-search.test.ts
@@ -1,47 +1,12 @@
 import { config } from "dotenv";
 import { describe } from "@jest/globals";
 import { getPlaceAutocomplete } from "../src/maps-api";
-import { getAutoCompleteDetails } from "../src";
 import { SearchApiError } from "../src/errors";
 
 config();
 
 // These are end-to-end tests and need an api key
-describe("Tomtom Places E2E Tests", () => {
-  describe("getAutoCompleteDetails", () => {
-    it("returns a promise", () => {
-      const res = getAutoCompleteDetails("Charlotte Street");
-      expect(res).toBeInstanceOf(Promise);
-    });
-
-    it("returns an array", async () => {
-      const res = await getAutoCompleteDetails("Charlotte Street");
-      expect(res).toBeInstanceOf(Array);
-    });
-
-    it("can fetch from the autocomplete api", async () => {
-      const res = await getAutoCompleteDetails("Charlotte Street");
-      const firstRes = res[0];
-      expect(firstRes).toHaveProperty("placeId");
-      expect(firstRes).toHaveProperty("streetNumber");
-      expect(firstRes).toHaveProperty("countryCode");
-      expect(firstRes).toHaveProperty("country");
-      expect(firstRes).toHaveProperty("freeformAddress");
-      expect(firstRes).toHaveProperty("municipality");
-    });
-
-    it("handles no results", async () => {
-      const res = await getAutoCompleteDetails("asfasffasfasafsafs");
-      expect(res).toBeNull(); //toStrictEqual([]);
-    });
-
-    it("handles error", async () => {
-      jest.spyOn(console, "error").mockImplementation();
-      await getAutoCompleteDetails("");
-      expect(console.error).toHaveBeenCalled();
-    });
-  });
-
+describe("Tomtom Places API E2E Tests", () => {
   describe("getPlaceAutocomplete", () => {
     it("returns a promise", () => {
       const res = getPlaceAutocomplete(
@@ -64,7 +29,7 @@ describe("Tomtom Places E2E Tests", () => {
         process.env.TOMTOM_API_KEY,
         "asfasffasfasafsafs"
       );
-      expect(res).toBeNull(); //toStrictEqual([]);
+      expect(res).toBeNull();
     });
 
     it("handles error", async () => {
