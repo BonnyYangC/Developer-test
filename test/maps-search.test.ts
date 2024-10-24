2beb50890400f5a428bed42f8aafb1431abf55e9
Bonny Yang <32720471+BonnyYangC@users.noreply.github.com>
2024-10-24 15:03:00 +1100

    Update maps-search.test.ts

--- test/maps-search.test.ts
+++ test/maps-search.test.ts
@@ -46,7 +46,7 @@ describe("Tomtom Places E2E Tests", () => {
     it("returns a promise", () => {
       const res = getPlaceAutocomplete(
         process.env.TOMTOM_API_KEY,
-        "Charlotte Street"
+        "Kerry Road"
       );
       expect(res).toBeInstanceOf(Promise);
     });
@@ -54,7 +54,7 @@ describe("Tomtom Places E2E Tests", () => {
     it("returns an array", async () => {
       const res = await getPlaceAutocomplete(
         process.env.TOMTOM_API_KEY,
-        "Charlotte Street"
+        "Kerry Road"
       );
       expect(res).toBeInstanceOf(Array);
     });
@@ -64,7 +64,7 @@ describe("Tomtom Places E2E Tests", () => {
         process.env.TOMTOM_API_KEY,
         "asfasffasfasafsafs"
       );
-      expect(res).toBeNull(); //toStrictEqual([]);
+      expect(res).toBeNull();
     });
 
     it("handles error", async () => {
