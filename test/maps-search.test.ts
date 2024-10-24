307b74de69cc3845ae8227e0ebe3bdcf030cc620
BonnyYangC <bonny.yang1981@gmail.com>
2024-10-24 13:34:34 +1100

    add test cases

--- test/maps-search.test.ts
+++ test/maps-search.test.ts
@@ -1,40 +1,76 @@
-import { config } from 'dotenv'
-import { describe } from '@jest/globals'
-import { getPlaceAutocomplete } from '../src/maps-api'
-import { getAutoCompleteDetails } from '../src'
+import { config } from "dotenv";
+import { describe } from "@jest/globals";
+import { getPlaceAutocomplete } from "../src/maps-api";
+import { getAutoCompleteDetails } from "../src";
+import { SearchApiError } from "../src/errors";
 
 config();
 
 // These are end-to-end tests and need an api key
-describe('Tomtom Places E2E Tests', () => {
-    describe('getAutoCompleteDetails', () => {
-        it ('returns a promise', () => {
-            const res = getAutoCompleteDetails('Charlotte Street')
-            expect(res).toBeInstanceOf(Promise)
-        })
+describe("Tomtom Places E2E Tests", () => {
+  describe("getAutoCompleteDetails", () => {
+    it("returns a promise", () => {
+      const res = getAutoCompleteDetails("Charlotte Street");
+      expect(res).toBeInstanceOf(Promise);
+    });
 
-        it('can fetch from the autocomplete api', async () => {
-            const res = await getAutoCompleteDetails('Charlotte Street')
-            const firstRes = res[0];
-            expect(firstRes).toHaveProperty('placeId')
-            expect(firstRes).toHaveProperty('streetNumber')
-            expect(firstRes).toHaveProperty('countryCode')
-            expect(firstRes).toHaveProperty('country')
-            expect(firstRes).toHaveProperty('freeformAddress')
-            expect(firstRes).toHaveProperty('municipality')
-        })
-    })
+    it("returns an array", async () => {
+      const res = await getAutoCompleteDetails("Charlotte Street");
+      expect(res).toBeInstanceOf(Array);
+    });
 
-    describe('getPlaceAutocomplete', () => {
+    it("can fetch from the autocomplete api", async () => {
+      const res = await getAutoCompleteDetails("Charlotte Street");
+      const firstRes = res[0];
+      expect(firstRes).toHaveProperty("placeId");
+      expect(firstRes).toHaveProperty("streetNumber");
+      expect(firstRes).toHaveProperty("countryCode");
+      expect(firstRes).toHaveProperty("country");
+      expect(firstRes).toHaveProperty("freeformAddress");
+      expect(firstRes).toHaveProperty("municipality");
+    });
 
-        it('handles no results', async () => {
-            const res = await getPlaceAutocomplete(process.env.TOMTOM_API_KEY, 'asfasffasfasafsafs');
-            expect(res).toStrictEqual([])
-        })
+    it("handles no results", async () => {
+      const res = await getAutoCompleteDetails("asfasffasfasafsafs");
+      expect(res).toBeNull(); //toStrictEqual([]);
+    });
 
-        it('handles error', async () => {
-            expect(getPlaceAutocomplete(process.env.TOMTOM_API_KEY, '')).rejects.toThrow()
-        })
-    })
+    it("handles error", async () => {
+      jest.spyOn(console, "error").mockImplementation();
+      await getAutoCompleteDetails("");
+      expect(console.error).toHaveBeenCalled();
+    });
+  });
 
-})
+  describe("getPlaceAutocomplete", () => {
+    it("returns a promise", () => {
+      const res = getPlaceAutocomplete(
+        process.env.TOMTOM_API_KEY,
+        "Charlotte Street"
+      );
+      expect(res).toBeInstanceOf(Promise);
+    });
+
+    it("returns an array", async () => {
+      const res = await getPlaceAutocomplete(
+        process.env.TOMTOM_API_KEY,
+        "Charlotte Street"
+      );
+      expect(res).toBeInstanceOf(Array);
+    });
+
+    it("handles no results", async () => {
+      const res = await getPlaceAutocomplete(
+        process.env.TOMTOM_API_KEY,
+        "asfasffasfasafsafs"
+      );
+      expect(res).toBeNull(); //toStrictEqual([]);
+    });
+
+    it("handles error", async () => {
+      expect(
+        getPlaceAutocomplete(process.env.TOMTOM_API_KEY, "")
+      ).rejects.toThrow(SearchApiError);
+    });
+  });
+});
