23e50bb0928cb0da61c599c5aaef692f60189179
BonnyYangC <bonny.yang1981@gmail.com>
2024-11-05 23:32:10 +1100

    add server side code

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 const { MongoClient } = require("mongodb")
 
 let dbConnection
+const db_uri = process.env.MONGODB_URI || 'mongodb://localhost:27017'
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect('mongodb://localhost:27017/Issue')
+    MongoClient.connect(`${db_uri}/Issue`)
       .then((client) => {
         dbConnection = client.db()
         return cb()
