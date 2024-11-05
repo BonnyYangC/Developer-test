c04e6f91135f8de42bc933278080666c760e0562
BonnyYangC <bonny.yang1981@gmail.com>
2024-11-05 17:03:00 +1100

    add body-parser

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import { config } from "dotenv";
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 
 config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Use body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, GhostLink!");
+});
+
+// Define a route
+app.post('/api/data', (req: Request, res: Response) => {
+  console.log(req.body); // Parsed data from body
+  res.send('Data received');
 });
 
 app.listen(port, () => {
